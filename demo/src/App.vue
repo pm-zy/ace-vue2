07d102012f75b2b9754e06dd0964078bfdd7af6e
panming <mingpan@sohu-inc.com>
2017-05-16 15:45:51 +0800

    m

---
+++ demo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h2>Ace Editor</h2>
+    <editor > </editor>
+  </div>
+</template>
+
+<script>
+import editor from '../../index.js'
+import 'brace/mode/javascript'
+import 'brace/theme/chrome'
+export default {
+  name: 'app',
+  component: {
+      editor
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
---
+++ index.js
@@ -0,0 +1,66 @@
+var ace = require('brace');
+
+module.exports = {
+    template: '<div :style="{height: height, width: width}"></div>',
+
+    props: {
+        content: {
+            type: String,
+            required: true
+        },
+        lang: String,
+        theme: {
+            type: String,
+            default: 'chrome'
+        },
+        height: {
+            type: String,
+            default: '400px'
+        },
+        width: {
+            type: String,
+            default: '100%'
+        },
+        sync: {
+            type: Boolean,
+            default: false
+        },
+        options: Object,
+    },
+
+    data: function () {
+        return {
+            editor: null,
+        };
+    },
+
+    mounted: function () {
+        var vm = this;
+        var lang = vm.lang;
+        var theme = vm.theme;
+        var editor = vm.editor = ace.edit(vm.$el);
+        var options = vm.options;
+        editor.$blockScrolling = Infinity;
+        editor.getSession().setMode('ace/mode/' + lang);
+        editor.setTheme('ace/theme/' + theme);
+        editor.setValue(vm.content, 1);
+        editor.setOptions(options);
+        editor.on('change', function () {
+          vm.$parent.$emit('editor-update', editor.getValue());
+        });
+    },
+
+    watch: {
+        content: function (newContent) {
+            var vm = this;
+            if (vm.sync) {
+                vm.editor.setValue(newContent, 1);
+            }
+        },
+
+        theme: function (newTheme) {
+            var vm = this;
+            vm.editor.setTheme('ace/theme/' + newTheme);
+        }
+    }
+};
