0e386ce7d911f04cfff49ee9e8906f25ddcea9fe
pandapm <panda_m@126.com>
2017-05-16 17:08:23 +0800

    add demo

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,39 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h2>Ace Editor</h2>
-    <editor > </editor>
-  </div>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <h2>Ace Editor</h2>
+        <editor height="200px" :content="content" lang="javascript"> </editor>
+        <div class="btn">
+            显示代码
+        </div>
+    </div>
 </template>
 
 <script>
-import editor from '../../index.js'
+import editor from 'ace-vue2'
 import 'brace/mode/javascript'
 import 'brace/theme/chrome'
 export default {
-  name: 'app',
-  component: {
-      editor
-  },
-  data () {
-    return {
+    name: 'app',
+    components: {
+        editor
+    },
+    data () {
+        return {
+            content: '在此处写代码'
+        }
+    },
+    methods: {
+        
     }
-  }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 
 h1, h2 {
-  font-weight: normal;
+    font-weight: normal;
 }
 
 </style>
